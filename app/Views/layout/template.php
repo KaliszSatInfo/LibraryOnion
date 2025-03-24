7d5ae64aa5cd346bd7eb7895a8f7e2a8bf1f03fa
KaliszSatInfo <jan.kalisz007@gmail.com>
2025-03-24 10:01:06 +0100

    Created a layout folder where template and assets are located. (composer require twbs/bootstrap to populate vendor with twbs)

---
+++ app/Views/layout/template.php
@@ -0,0 +1,13 @@
+<!Doctype html>
+<html>
+    <head>
+        <title>Katabáze Dnih</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+        <?= $this->include("layout/assets");?> 
+    </head>
+
+    <body>
+        <?= $this->renderSection("content");?>
+    </body>
+</html>
