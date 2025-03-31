4d8385a96f268efe43b9b47bd6191cf1a448a06d
frystak_jan <janfrystak@gmail.com>
2025-03-31 11:23:57 +0200

    Primitive translation technology v.1 (move into config)

--- app/Database/Seeds/SeederBook.php
+++ app/Database/Seeds/SeederBook.php
@@ -7,14 +7,20 @@ use Faker\Factory;
 
 class SeederBook extends Seeder
 {
+    public function translate($q, $sl, $tl){
+        $res= file_get_contents("https://translate.googleapis.com/translate_a/single?client=gtx&ie=UTF-8&oe=UTF-8&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&dt=at&sl=".$sl."&tl=".$tl."&hl=hl&q=".urlencode($q), $_SERVER['DOCUMENT_ROOT']."/transes.html");
+        $res=json_decode($res);
+        return $res[0][0][0];
+    }
+    
     public function run()
     {
         $faker = Factory::create();
 
-        for($i = 0; $i < 5000; $i++){
+        for($i = 0; $i < 500; $i++){
             $data = [
-                'title' => $faker->sentence(4),
-                'description' => $faker->paragraph(8),
+                'title' => $this->translate($faker->sentence(4), "la", "en"),
+                'description' => $this->translate($faker->paragraph(8), "la", "en"),
                 'published_date' => $faker->date(),
                 'cover_image' => 'https://picsum.photos/seed/' . uniqid() . '/400/600',
                 'isbn' => $faker->isbn13()
