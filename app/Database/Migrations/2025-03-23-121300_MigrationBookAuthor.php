a81528b78877fe9e72957e64c7697cbe3a9eac7d
KaliszSatInfo <jan.kalisz007@gmail.com>
2025-03-31 10:52:29 +0200

    Updated Migrations

--- app/Database/Migrations/2025-03-23-121300_MigrationBookAuthor.php
+++ app/Database/Migrations/2025-03-23-121300_MigrationBookAuthor.php
@@ -9,9 +9,11 @@ class MigrationBookAuthor extends Migration
     public function up()
     {
         $this->forge->addField([
+            'id' => ['type' => 'INT', 'unsigned' => true, 'auto_increment' => true],
             'book_id' => ['type' => 'INT', 'unsigned' => true],
             'author_id' => ['type' => 'INT', 'unsigned' => true]
         ]);
+        $this->forge->addKey('id', true);
         $this->forge->addForeignKey('book_id', 'book', 'id');
         $this->forge->addForeignKey('author_id', 'author', 'id');
         $this->forge->createTable('book_author');
